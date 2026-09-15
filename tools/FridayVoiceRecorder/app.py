64aeb6dd0e91ba6ee72b91bc21901a097b14b8bb
holy-moly-l <kos099931@gmail.com>
2026-09-15 22:37:51 +0300

    Enable recorder runtime fixes

--- tools/FridayVoiceRecorder/app.py
+++ tools/FridayVoiceRecorder/app.py
@@ -1,4 +1,7 @@
-from src.friday_voice_recorder import main
+from src import friday_voice_recorder as fvr
+from src.runtime_fixes import install_hotfixes
+
+install_hotfixes(fvr)
 
 if __name__ == "__main__":
-    main()
+    fvr.main()
